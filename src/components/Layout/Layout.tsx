cde01383be6dc854a157c219beed866ae24a07cc
Александр Козлов <aleksandrkozlov@MacBook-Pro-Aleksandr-2.local>
2025-04-15 00:27:17 +0300

    fix ci 7

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1 className={styles.title}>React app (client side rendering)</h1>
+        <h1 className={styles.title}>React app (client side rendering) cd</h1>
       </header>
       <main className={styles.content}>{children}</main>
     </div>
